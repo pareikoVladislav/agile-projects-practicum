4144b27d2733861d7ff9165dbee081a584995699
Izzet <yunusovizzet6@gmail.com>
2025-05-02 10:03:30 +0200

    task 1

--- projects/serializers/projects.py
+++ projects/serializers/projects.py
@@ -45,3 +45,64 @@ class AllProjectFilesSerializer(serializers.ModelSerializer):
         exclude = ('file',)
 
 
+class CreateUpdateFileSerializer(serializers.ModelSerializer):
+    project_id = serializers.PrimaryKeyRelatedField(
+        write_only=True,
+        queryset=Project.objects.all()
+    )
+
+    class Meta:
+        model = ProjectFile
+        fields = ["project_id"]
+
+
+    def validate(self, attrs):
+        file_context = self.context.get("raw_file")
+
+        if not file_context:
+            raise serializers.ValidationError("Файл обязателен")
+
+        if not file_context.name.isascii():
+            raise serializers.ValidationError("Название не соответствует параметрам ASCII")
+
+        if not check_extension(file_context.name):
+            raise serializers.ValidationError(f"Расширение файлов должно быть одним из {ALLOWED_EXTENSIONS}")
+
+        if not check_file_size(file_context):
+            raise serializers.ValidationError("Размер превышает 2 Мегабайта")
+
+        return attrs
+
+    def create(self, validated_data):
+        proj_id = validated_data.pop("project_id")
+
+        raw_file = self.context.get("raw_file")
+
+        file_name = raw_file.name
+
+        file_path = create_file_path(proj_id.name, file_name)
+
+        os.makedirs(name=os.path.dirname(file_path), exist_ok=True)
+
+        proj_file = ProjectFile.objects.create(
+            name= file_name,
+            file= file_path,
+        )
+
+        proj_file.projects.add(proj_id)
+
+        return proj_file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
